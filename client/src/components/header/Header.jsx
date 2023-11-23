c393c3f24b13d02c7ece792b5d06bca14a7c71d2
Wassim Al Haraki <wassimalharaki@gmail.com>
2023-11-23 11:47:34 +0200

    initial commit

---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,13 @@
+import "./header.css";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <img
+        className="headerImg"
+        src="https://miro.medium.com/v2/resize:fit:720/format:webp/1*A94QQoJ05Rg8q3JJo2K2Fg.png"
+        alt=""
+      />
+    </div>
+  );
+}
---
+++ client/src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,30 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import "./sidebar.css";
+
+export default function Sidebar() {
+  const [cats, setCats] = useState([]);
+
+  useEffect(() => {
+    const getCats = async () => {
+      const res = await axios.get("/categories");
+      setCats(res.data);
+    };
+    getCats();
+  }, []);
+  return (
+    <div className="sidebar">
+      <div className="sidebarItem">
+        <span className="sidebarTitle">ABOUT ME</span>
+        <img
+          src="http://localhost:3000/pfp.jpeg"
+          alt=""
+        />
+        <p style={{marginLeft: 30}}>
+          My name is Wassim Al Haraki. I am a senior Computer Science student set to graduate in 2024.
+        </p>
+      </div>
+    </div>
+  );
+}
